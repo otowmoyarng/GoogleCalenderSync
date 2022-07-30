1a2df2501e1c67ef5466bb854ed48cb0218c22da
otowmoyarng <hidemusiam@gmail.com>
2022-07-30 10:55:04 +0900

    テスト関数名を訂正

--- test/Common.js
+++ test/Common.js
@@ -1,13 +1,13 @@
-function TestGetCurrentYmd() {
+function TestGetYmd() {
     console.log(`dateなし:${Common.GetCurrentYmd()}`);
     const testdate = new Date();
     testdate.setDate(testdate.getDate() - 1);
     console.log(`dateあり:${Common.FormatDate(testdate)}`);
 }
 
-function TestGetCurrentTime() {
+function TestGetTime() {
     console.log(`dateなし:${Common.GetCurrentTime()}`);
     const testdate = new Date();
-    testdate.setDate(testdate.getDate() - 1);
+    testdate.setHours(testdate.getHours() === 0 ? 24 : testdate.getHours() - 1);
     console.log(`dateあり:${Common.FormatTime(testdate)}`);
 }
