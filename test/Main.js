7bfe6c4449ca429d3ced758040d7b2b2780b2c81
otowmoyarng <hidemusiam@gmail.com>
2022-08-01 00:47:05 +0900

    日付を範囲指定して実行する

--- test/Main.js
+++ test/Main.js
@@ -2,3 +2,14 @@ function MainTest() {
     const date = new Date(2022, 6, 29, 12, 34, 56);
     Main(date);
 }
+
+function RangeTest() {
+    const startDate = new Date(2022, 1, 1);
+    const endDate = new Date(2022, 2, 1);
+    let targetDate = startDate;
+    while (targetDate <= endDate) {
+        console.log("Date is ", Common.FormatDate(targetDate));
+        Main(targetDate);
+        targetDate.setDate(targetDate.getDate() + 1);
+    }
+}
